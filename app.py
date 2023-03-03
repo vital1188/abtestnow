d643e49e413e8936ef7078eda40948483aa3c8a4
rails new my_shopify_app <viotrip@gmail.com>
2023-03-02 22:59:20 -0500

    ai

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
+import openai
 import os
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request
 from statsmodels.stats.proportion import proportions_ztest
 import math
 
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+model_engine = "text-davinci-003"
+
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['STATIC_FOLDER'] = 'static'
+
+
+def generate_explanation(control_conversion_rate, variation_conversion_rate, relative_improvement, significance_level):
+    prompt = f"Control Conversion Rate: {control_conversion_rate}%. Variation Conversion Rate: {variation_conversion_rate}%. Relative Improvement: {relative_improvement}%. Significance Level: {significance_level}."
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    explanation = response.choices[0].text.strip()
+    return explanation
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -33,6 +55,9 @@ def calculate():
         else:
             significance_level = 'not significant (p >= 0.05)'
 
+        openai_explanation = generate_explanation(
+            control_conversion_rate, variation_conversion_rate, relative_improvement, significance_level)
+
         return render_template('index.html',
                                control_conversion_rate=control_conversion_rate,
                                variation_conversion_rate=variation_conversion_rate,
@@ -42,7 +67,8 @@ def calculate():
                                control_visitors_input=control_visitors,
                                control_conversions_input=control_conversions,
                                variation_visitors_input=variation_visitors,
-                               variation_conversions_input=variation_conversions)
+                               variation_conversions_input=variation_conversions,
+                               openai_explanation=openai_explanation)
 
     # If request method is GET, render the template with empty input fields
     return render_template('index.html',
